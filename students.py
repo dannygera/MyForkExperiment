826230a2356e7c3cfdf2e5596828d6a9db235075
Danny Geraerts <danny1973@gmail.com>
2025-05-25 14:49:00 +0200

    students.py created
    example file how to create a list of email addresses of students

---
+++ students.py
@@ -0,0 +1,16 @@
+
+contacts = {
+    'number': 4,
+    'students':
+        [
+            {'name':'jessica', 'email':'jessica@voorbeeld.com'},
+            {'name':'danny', 'email': 'danny@voorbeeld.com'},
+            {'name':'marlon', 'email': 'marlon@voorbeeld.com'},
+            {'name':'quinty', 'email': 'quity@vorbeeld.com'}
+        ]
+}
+
+print('student emails:')
+
+for student in contacts['students']:
+    print(student['email'])
